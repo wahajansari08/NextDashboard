b2298ecc5aa7c100c9dac30ff5bb70bbb15e2afb
wahajansari08 <wahajansari08@gmail.com>
2025-05-23 16:12:30 +0500

    52 mins 13 secs

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,124 @@
+import { Home, Inbox, Calendar, Search, Settings, User2, ChevronUp, Plus, Projector } from "lucide-react"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator, SidebarGroupAction } from './ui/sidebar'
+import Link from 'next/link'
+import Image from "next/image"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { DropdownMenuItem } from "./ui/dropdown-menu"
+
+const items=[
+  {
+    title:"home",
+    url:"/",
+    icon:Home
+  },
+  {
+    title:"Inbox",
+    url:"#",
+    icon:Inbox
+  },
+  {
+    title:"Calendar",
+    url:"#  ",
+    icon:Calendar
+  },
+  {
+    title:"Search",
+    url:"#  ",
+    icon:Search
+  },
+  {
+    title:"Settings",
+    url:"#  ",
+    icon:Settings
+  },
+]
+
+const AppSidebar = () => {
+  
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="py-4">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <Image src="/vercel.svg" alt="logo" width={20} height={20}/>
+                <span>Dashboard</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarSeparator />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Applicaiton
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item)=>(
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Project
+          </SidebarGroupLabel>
+          <SidebarGroupAction>
+            <Plus /> <span className="sr-only">Add Project</span>
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href='/#'>
+                    <Projector />
+                    See All Projectors
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href='/#'>
+                    <Plus />
+                    Add Project
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> John Doe <ChevronUp className="ml-auto"/>
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem> Account </DropdownMenuItem>
+                <DropdownMenuItem> Setting </DropdownMenuItem>
+                <DropdownMenuItem> Sign out </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
+
+export default AppSidebar
